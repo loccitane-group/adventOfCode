b092e3e79ff88bcc841ce2f1dd622839b4416934
Ismaïl NGUYEN <nguyen.ismail@gmail.com>
2023-12-02 23:41:37 +0800

    move day 1 and 2 into separate folder

---
+++ day2/day2.js
@@ -0,0 +1,12 @@
+function findPossibleGames(gameRecords, { nbGreenCubes, nbBlueCubes, nbRedCubes }) {
+    return ['Game 1', 'Game 2', 'Game 3', 'Game 4', 'Game 5']
+}
+
+function sumOfGameIds(gameIds) {
+    return 15
+}
+
+module.exports = {
+    sumOfGameIds,
+    findPossibleGames
+}
---
+++ day2/day2.test.js
@@ -0,0 +1,23 @@
+const {
+    sumOfGameIds,
+    findPossibleGames
+} = require('./day2')
+
+// test('adds 1 + 2 to equal 3', () => {
+//     const gameRecords = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+//     Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+//     Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+//     Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+//     Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`
+
+    
+
+//     expect(sum(1, 2)).toBe(3)
+// })
+
+
+test('Sum of Game Ids', () => {
+    const gameIds = ['Game 1', 'Game 2', 'Game 3', 'Game 4', 'Game 5']
+
+    expect(sumOfGameIds(gameIds)).toBe(15)
+})
