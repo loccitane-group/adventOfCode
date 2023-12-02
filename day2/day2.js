abd3f765cbe259b95ec1a8b39bd32ba8bb31d86e
Ismaïl NGUYEN <nguyen.ismail@gmail.com>
2023-12-03 00:45:37 +0800

    calculate power of minimum required set of cubes

--- day2/day2.js
+++ day2/day2.js
@@ -61,7 +61,63 @@ function sumOfGameIds(gameIds) {
     return sum
 }
 
+function sumOfPowers(powers) {
+    let sum = 0
+
+    for (let i = 0; i < powers.length; i++) {
+        const power = powers[i]
+        sum += power
+    }
+
+    return sum
+
+}
+
+function findMinimumSetOfCubes(rawGameRecords) {
+    const powers = []
+
+    const gameRecordLines = rawGameRecords.split('\n')
+    const gameRecords = gameRecordLines.map(line => ({
+        gameId: line.split(':')[0].trim(),
+        rounds: line.split(':')[1].trim().split(';')
+    }))
+
+    for (let i = 0; i < gameRecords.length; i++) {
+        const gameRecord = gameRecords[i]
+        const rounds = gameRecord.rounds
+        let requiredNbGreenCubes = 0
+        let requiredNbBlueCubes = 0
+        let requiredNbRedCubes = 0
+        
+        for (let j = 0; j < rounds.length; j++) {
+            const round = rounds[j]
+            const cubes = round.split(',').map(cube => cube.trim())
+
+            for (let k = 0; k < cubes.length; k++) {
+                const cube = cubes[k]
+                const color = cube.split(' ')[1]
+                const nbCubes = parseInt(cube.split(' ')[0])
+
+                if (color === 'green' && nbCubes > requiredNbGreenCubes) {
+                    requiredNbGreenCubes = nbCubes
+                } else if (color === 'blue' && nbCubes > requiredNbBlueCubes) {
+                    requiredNbBlueCubes = nbCubes
+                } else if (color === 'red' && nbCubes > requiredNbRedCubes) {
+                    requiredNbRedCubes = nbCubes
+                }
+            }
+        }
+
+        powers.push(requiredNbGreenCubes * requiredNbBlueCubes * requiredNbRedCubes)
+    }
+    
+    return powers
+}
+
+
 module.exports = {
     sumOfGameIds,
-    findPossibleGames
+    findPossibleGames,
+    sumOfPowers,
+    findMinimumSetOfCubes
 }
