9b89624b8cf8b9b7890f803b95fc1c91cc1357bf
Ismaïl NGUYEN <nguyen.ismail@gmail.com>
2023-12-04 00:33:19 +0800

    day 3 part 1

--- day3/day3.js
+++ day3/day3.js
@@ -8,35 +8,13 @@ function sum(numbers) {
     return sum
 }
 
-/*
+function isValidIndex(index, lines) {
+    return index >= 0 && index < lines.length
+}
 
-    given the following engine schematic:
-    467..114..
-    ...*......
-    ..35..633.
-    ......#...
-    617*......
-    .....+.58.
-    ..592.....
-    ......755.
-    ...$.*....
-    .664.598..
+const isNumber = x => !isNaN(parseFloat(x)) && isFinite(x)
+const isSymbol = x => x && !isNumber(x) && x !== '.'
 
-    should find any number that is adjacent  to any symbol except for the dot
-    and return them in an array
-
-    in the above example the result should be:
-    [
-        467,
-        35,
-        633,
-        617,
-        592,
-        755,
-        664,
-        598
-    ]
-*/
 function findAdjacentNumbers (engineSchematic) {
     const numbers = []
     const partNumbers = []
@@ -52,59 +30,46 @@ function findAdjacentNumbers (engineSchematic) {
             const char = engineSchematicLine[j]
     
             // While reading the number store it in a buffer
-            if (isNumber(parseInt(char))) {
+            if (isNumber(char)) {
                 tempNumberBuffer += char
             }
-            // If we encounter a dot, we know the number is over
-            else {
-                if (tempNumberBuffer.length > 0) {
-                    numbers.push(parseInt(tempNumberBuffer))
-    
-                    let hasAdjacentSymbol = false
-                    // Check right
-                    hasAdjacentSymbol = hasAdjacentSymbol || isSymbol(engineSchematicLine[j])
-                    // Check left
-                    hasAdjacentSymbol = hasAdjacentSymbol || isSymbol(engineSchematicLine[j - tempNumberBuffer.length - 1])
-                    // Check diagonal top right
-                    hasAdjacentSymbol = hasAdjacentSymbol
-                                            || (engineSchematicLines[i-1] && engineSchematicLines[i-1][j] && isSymbol(engineSchematicLines[i-1][j]))
-                    // Check diagonal bottom right
-                    hasAdjacentSymbol = hasAdjacentSymbol
-                                        || (engineSchematicLines[i+1] && engineSchematicLines[i+1][j] && isSymbol(engineSchematicLines[i+1][j]))
-                    // Check diagonal top left
-                    hasAdjacentSymbol = hasAdjacentSymbol
-                                        || (engineSchematicLines[i-1] && engineSchematicLines[i-1][j - tempNumberBuffer.length - 1] && isSymbol(engineSchematicLines[i-1][j - tempNumberBuffer.length - 1]))
-                    // Check diagonal top left
-                    hasAdjacentSymbol = hasAdjacentSymbol
-                                        || (engineSchematicLines[i+1] && engineSchematicLines[i+1][j - tempNumberBuffer.length - 1] && isSymbol(engineSchematicLines[i+1][j - tempNumberBuffer.length - 1]))
-    
-                    // Check bottom and top symbols for each number of the tempBufferNumber
-                    for (var k = 0; k < tempNumberBuffer.length; k++) {
-                        // Check top line
-                        hasAdjacentSymbol = hasAdjacentSymbol
-                                            || (engineSchematicLines[i-1] && engineSchematicLines[i-1][j - k - 1] && isSymbol(engineSchematicLines[i-1][j - k - 1]))
 
-                        // Check bottom line
-                        hasAdjacentSymbol = hasAdjacentSymbol
-                                            || (engineSchematicLines[i+1] && engineSchematicLines[i+1][j - k - 1] && isSymbol(engineSchematicLines[i+1][j - k - 1]))
+            let isEndOfLine = j == engineSchematicLine.length - 1
+
+            if (isEndOfLine || (!isNumber(char) && tempNumberBuffer.length > 0)) {
+                let hasAdjacentSymbol = false
+
+                for (var k = -1; k < 2; k++) {
+                    for (var l = -tempNumberBuffer.length-1; l < 1; l++) {
+                        let surroundedLine = i+k
+                        let surroundedRow = j+l
+                        let isValidLine = isValidIndex(surroundedLine, engineSchematicLines)
+                        let isValidRow = isValidIndex(surroundedRow, engineSchematicLine)
+
+                        if( isValidLine && isValidRow) {
+                            let surroundingChar = engineSchematicLines[surroundedLine][surroundedRow]
+                            let foundSymbol = isSymbol(surroundingChar)
+
+                            if (foundSymbol) {
+                                hasAdjacentSymbol = true
+                            }
+                        }
                     }
-
-                    if (hasAdjacentSymbol) {
-                        partNumbers.push(parseInt(tempNumberBuffer))
-                    }
-
-                    tempNumberBuffer = ''
                 }
+
+                numbers.push(parseInt(tempNumberBuffer))
+
+                if (hasAdjacentSymbol) {
+                    partNumbers.push(parseInt(tempNumberBuffer))
+                }
+
+                tempNumberBuffer = ''
             }
         }
     }
 
     return partNumbers
 }
-
-const isNumber = x => Number.isInteger(x)
-const isDot = x => x === '.'
-const isSymbol = x => x && !isNumber(x) && !isDot(x)
 
 module.exports = {
     sum,
