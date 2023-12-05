a1417def0d7bda00b8f4bf3bf09dc18568468797
Jordan Afonso <jordan.afonso.ext@loccitane.com>
2023-12-05 14:01:26 +0100

    sum all winning game ids

--- day2/test/day2.spec.ts
+++ day2/test/day2.spec.ts
@@ -1,4 +1,5 @@
 import { describe, expect, it } from 'vitest';
+import { games } from '../src';
 
 const getGameId = (gameName: string): number => {
     const [, gameId] = gameName.split(' ');
@@ -14,12 +15,12 @@ const parseGameSets = (setsDescription: string) =>
 
 const getCubeAmount = (setsDescription: string, cubeColor: string) => {
     const redCube = parseGameSets(setsDescription);
-    return redCube.reduce((accumulator, currentSet) => {
+    return redCube.reduce((maxAmount, currentSet) => {
         currentSet.forEach((cubeSelection) => {
             const [quantity, color] = cubeSelection.split(' ');
-            if (color === cubeColor) accumulator += Number(quantity);
+            if (color === cubeColor) maxAmount = maxAmount > Number(quantity) ? maxAmount : Number(quantity);
         });
-        return accumulator;
+        return maxAmount;
     }, 0);
 };
 
@@ -50,6 +51,21 @@ const isGamePossible = (game: Game, bagContent: BagContent): boolean => {
     return !(game.red > bagContent.red || game.green > bagContent.green || game.blue > bagContent.blue);
 };
 
+const sumInvalidGamesId = (gamesRecord: string[]): number => {
+    const badContent: BagContent = {
+        red: 12,
+        green: 13,
+        blue: 14,
+    };
+    return gamesRecord.reduce((accumulator, gameRecord) => {
+        const currentGame: Game = parseGame(gameRecord);
+        if (isGamePossible(currentGame, badContent)) {
+            accumulator += currentGame.id;
+        }
+        return accumulator;
+    }, 0);
+};
+
 describe('Day 2 ADVOC2023', () => {
     it('Should get the game id', () => {
         expect(getGameId('Game 1')).toEqual(1);
@@ -67,17 +83,17 @@ describe('Day 2 ADVOC2023', () => {
         ]);
     });
     it('Should get the red cube amount when game contains multiple sets', () => {
-        expect(getCubeAmount(' 2 blue, 3 red; 2 blue, 3 red', 'red')).toEqual(6);
+        expect(getCubeAmount(' 2 blue, 3 red; 2 blue, 3 red', 'red')).toEqual(3);
     });
     it('Should get the blue cube amount when game contains multiple sets', () => {
-        expect(getCubeAmount(' 2 blue, 3 red; 2 blue, 3 red', 'blue')).toEqual(4);
+        expect(getCubeAmount(' 2 blue, 3 red; 4 blue, 3 red', 'blue')).toEqual(4);
     });
     it('Should parse a complete game', () => {
         expect(parseGame('Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green')).toEqual({
             id: 1,
-            red: 5,
-            green: 4,
-            blue: 9,
+            red: 4,
+            green: 2,
+            blue: 6,
         });
     });
     it('Should valid a valid game', () => {
@@ -107,5 +123,18 @@ describe('Day 2 ADVOC2023', () => {
             blue: 9,
         };
         expect(isGamePossible(game, bagContent)).toBe(false);
+    });
+    it('should solve record in doc', () => {
+        const gameRecord = [
+            'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green',
+            'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue',
+            'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red',
+            'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red',
+            'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green',
+        ];
+        expect(sumInvalidGamesId(gameRecord)).toEqual(8);
+    });
+    it('should solve game record of day2', () => {
+        expect(sumInvalidGamesId(games)).toEqual(2416);
     });
 });
