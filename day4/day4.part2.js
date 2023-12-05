552806b115007988328cc86701e163471ffd84aa
Ismaïl NGUYEN <nguyen.ismail@gmail.com>
2023-12-05 16:35:59 +0800

    fing the winning cards

--- day4/day4.part2.js
+++ day4/day4.part2.js
@@ -1,3 +1,5 @@
+const { parseCards } = require('./day4.part1')
+
 function calculatePoints (card) {
     //Example of card : Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 
@@ -18,14 +20,37 @@ function calculatePoints (card) {
         }
     })
 
-    // if there is no matches, return 0
-    if (numberOfMatches === 0) {
-        return 0
-    }
-
     return numberOfMatches
+}
+
+/*
+    Given the following 'cards':
+    Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+    Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+    Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+    Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+    Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+    Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+
+    And a starting index of 0
+
+    Should return an array with:
+    'Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19',
+    'Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1',
+    'Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83',
+    'Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36'
+*/
+function getWinningCards(cards, startingIndex) {
+    const cardsArray = parseCards(cards)
+
+    const cardToCalculate = cardsArray[startingIndex]
+    const numberOfMatches = calculatePoints(cardToCalculate)
+    const winningCards = cardsArray.slice(startingIndex+1, startingIndex+numberOfMatches+1)
+
+    return winningCards
 }
 
 module.exports = {
     calculatePoints,
+    getWinningCards
 }
