6d7f9efef8d5f1a738307044c60297f98496d02d
Jordan Afonso <jordan.afonso.ext@loccitane.com>
2023-12-05 09:14:42 +0100

    parse a complete game

--- day2/test/day2.spec.ts
+++ day2/test/day2.spec.ts
@@ -5,15 +5,15 @@ const getGameId = (gameName: string): number => {
     return Number(gameId);
 };
 
-const parseGameSets = (cubePickedInGame: string) =>
-    cubePickedInGame.split(';').map((set) => {
+const parseGameSets = (setsDescription: string) =>
+    setsDescription.split(';').map((set) => {
         return set.split(',').map((cubeSelection) => {
             return cubeSelection.trim();
         });
     });
 
-const getCubeAmount = (cubePickedInGame: string, cubeColor: string) => {
-    const redCube = parseGameSets(cubePickedInGame);
+const getCubeAmount = (setsDescription: string, cubeColor: string) => {
+    const redCube = parseGameSets(setsDescription);
     return redCube.reduce((accumulator, currentSet) => {
         currentSet.forEach((cubeSelection) => {
             const [quantity, color] = cubeSelection.split(' ');
@@ -21,6 +21,23 @@ const getCubeAmount = (cubePickedInGame: string, cubeColor: string) => {
         });
         return accumulator;
     }, 0);
+};
+
+type Game = {
+    id: number;
+    red: number;
+    green: number;
+    blue: number;
+};
+
+const parseGame = (gameDescription: string): Game => {
+    const [gameName, setsDescription] = gameDescription.split(':');
+    return {
+        id: getGameId(gameName),
+        red: getCubeAmount(setsDescription, 'red'),
+        green: getCubeAmount(setsDescription, 'green'),
+        blue: getCubeAmount(setsDescription, 'blue'),
+    };
 };
 
 describe('Day 2 ADVOC2023', () => {
@@ -44,5 +61,13 @@ describe('Day 2 ADVOC2023', () => {
     });
     it('Should get the blue cube amount when game contains multiple sets', () => {
         expect(getCubeAmount(' 2 blue, 3 red; 2 blue, 3 red', 'blue')).toEqual(4);
+    });
+    it('Should parse a complete game', () => {
+        expect(parseGame('Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green')).toEqual({
+            id: 1,
+            red: 5,
+            green: 4,
+            blue: 9,
+        });
     });
 });
