f4c35595276b80783fd4b40a8cf8d0d058966f64
Jordan Afonso <jordan.afonso.ext@loccitane.com>
2023-12-05 08:58:06 +0100

    manage game with multiple sets

--- day2/test/day2.spec.ts
+++ day2/test/day2.spec.ts
@@ -1,15 +1,26 @@
 import { describe, expect, it } from 'vitest';
 
 const getGameId = (gameName: string): number => {
-    return Number(gameName.split(' ')[1]);
+    const [, gameId] = gameName.split(' ');
+    return Number(gameId);
 };
 
+const parseGameSets = (cubePickedInGame: string) =>
+    cubePickedInGame.split(';').map((set) => {
+        return set.split(',').map((cubeSelection) => {
+            return cubeSelection.trim();
+        });
+    });
+
 const getRedCubeAmount = (cubePickedInGame: string) => {
-    const redCube = cubePickedInGame
-        .split(',')
-        .map((cubeSelection) => cubeSelection.trim())
-        .filter((cubeSelection) => cubeSelection.includes('red'));
-    return Number(redCube[0].split(' ')[0]);
+    const redCube = parseGameSets(cubePickedInGame);
+    return redCube.reduce((accumulator, currentSet) => {
+        currentSet.forEach((cubeSelection) => {
+            const [quantity, color] = cubeSelection.split(' ');
+            if (color === 'red') accumulator += Number(quantity);
+        });
+        return accumulator;
+    }, 0);
 };
 
 describe('Day 2 ADVOC2023', () => {
@@ -19,7 +30,16 @@ describe('Day 2 ADVOC2023', () => {
     it('Should get the red cube amount', () => {
         expect(getRedCubeAmount(' 2 red')).toEqual(2);
     });
-    it('Should get the red cube amount when other colors', () => {
+    it('Should get the red cube amount when set contains multiple colors', () => {
         expect(getRedCubeAmount(' 2 blue, 3 red')).toEqual(3);
+    });
+    it('Should correctly parse game sets', () => {
+        expect(parseGameSets(' 2 blue, 3 red; 2 blue, 3 red')).toEqual([
+            ['2 blue', '3 red'],
+            ['2 blue', '3 red'],
+        ]);
+    });
+    it('Should get the red cube amount when game contains multiple sets', () => {
+        expect(getRedCubeAmount(' 2 blue, 3 red; 2 blue, 3 red')).toEqual(6);
     });
 });
