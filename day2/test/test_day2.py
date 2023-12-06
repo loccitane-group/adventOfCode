da743632607b12597e35886f89b175674d14a705
Guillaume Duquesnay <guillaume.duquesnay@gmail.com>
2023-12-06 11:08:06 +0700

    (Refactor) factorisation of color checking

--- day2/test/test_day2.py
+++ day2/test/test_day2.py
@@ -1,63 +1,62 @@
 import re
+from unittest import skip
 
 RED_PATTERN = re.compile('(\d+) red')
 GREEN_PATTERN = re.compile('(\d+) green')
 BLUE_PATTERN = re.compile('(\d+) blue')
 
 
-def check_game(line):
-    pass
+def parse_pattern(pattern, round):
+    return pattern.search(round).groups()[0]
 
 
-def check_round_on_reds(round):
-    return int(parse_red_count(round)) <= 12
+def check_color(round, pattern, max):
+    return int(parse_pattern(pattern, round)) <= max
 
 
-def parse_red_count(round):
-    return RED_PATTERN.search(round).groups()[0]
-
-
-def parse_green_count(round):
-    return GREEN_PATTERN.search(round).groups()[0]
-
-
-
-def check_round_on_greens(round):
-    return int(parse_green_count(round)) <= 13
-
-
-def parse_blue_count(round):
-    return BLUE_PATTERN.search(round).groups()[0]
-
-
-
-def check_round_on_blues(round):
-    return int(parse_blue_count(round)) <= 14
+def check_round(round):
+    return check_color(round, RED_PATTERN, 12) \
+        and check_color(round, GREEN_PATTERN, 13) \
+        and check_color(round, BLUE_PATTERN, 14)
 
 
 class TestDay2_parsing:
     def test_round_fitting_12reds_single_color(self):
-        assert check_round_on_reds('12 red')
+        assert check_color('12 red', RED_PATTERN, 12)
 
     def test_round_not_fitting_12reds_single_color(self):
-        assert not check_round_on_reds('13 red')
+        assert not check_color('13 red', RED_PATTERN, 12)
 
     def test_round_fitting_12_reds_mixed_colors(self):
-        assert check_round_on_reds('12 red, 7 green')
-        assert check_round_on_reds('2 blue, 12 red, 7 green')
+        assert check_color('12 red, 7 green', RED_PATTERN, 12)
+        assert check_color('2 blue, 12 red, 7 green', RED_PATTERN, 12)
 
     def test_round_not_fitting_12_reds_mixed_colors(self):
-        assert not check_round_on_reds('13 red, 9 green')
-        assert not check_round_on_reds('2 blue, 13 red, 9 green')
+        assert not check_color('13 red, 9 green', RED_PATTERN, 12)
+        assert not check_color('2 blue, 13 red, 9 green', RED_PATTERN, 12)
 
     def test_round_fitting_13_greens_mixed_color(self):
-        assert check_round_on_greens('12 red, 13 green, 6 blue')
+        max = 13
+        pattern = GREEN_PATTERN
+        assert check_color('12 red, 13 green, 6 blue', pattern, max)
 
     def test_round_not_fitting_13_greens_mixed_color(self):
-        assert not check_round_on_greens('12 red, 14 green, 6 blue')
+        max = 13
+        pattern = GREEN_PATTERN
+        assert not check_color('12 red, 14 green, 6 blue', pattern, max)
 
     def test_round_fitting_14_blues_mixed_colorS(self):
-        assert check_round_on_blues('12 red, 13 green, 14 blue')
+        assert check_color('12 red, 13 green, 14 blue', BLUE_PATTERN, 14)
 
     def test_round_not_fitting_14_blues_mixed_colorS(self):
-        assert not check_round_on_blues('12 red, 13 green, 15 blue')
+        assert not check_color('12 red, 13 green, 15 blue', BLUE_PATTERN, 14)
+        
+    def test_round_fitting_missing_full_colors(self):
+        assert check_round('12 red, 13 green, 14 blue')
+
+    def test_round_not_fitting_one_color(self):
+        assert not check_round('12 red, 15 green, 14 blue')
+
+    @skip
+    def test_round_fitting_missing_red(self):
+        assert check_round('13 green, 14 blue')
